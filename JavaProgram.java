8b3d7923b318bf1dc8a5336834f3814a3b664ec9
pap88marios <pap88marios@gmail.com>
2018-11-17 13:05:27 +0200

    2nd commit

--- JavaProgram.java
+++ JavaProgram.java
@@ -1,6 +1,7 @@
 public class JavaProgram1 {
 
     public static void main() {
-        System.out.println("Hello world!")
+        System.out.println("Hello world!");
+        System.out.println("Hello world again!");
     }
 }
